f6fd89df3a623c08e20c90eccacb60a5b4db219b
yomad96 <57949254+yomad96@users.noreply.github.com>
2020-03-24 12:03:23 +0100

    add cave index page

--- projet_cave_a_vin/assets/js/app.js
+++ projet_cave_a_vin/assets/js/app.js
@@ -2,7 +2,7 @@
  * Welcome to your app's main JavaScript file!
  *
  * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
+ * (and its CSS file) in your base layout (add-cave-modal.html.twig).
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
@@ -10,6 +10,8 @@ import '../css/app.css';
 import '../css/_variables.scss'
 import '../css/_navbar-fixed.scss'
 import '../css/_navbar-fixed-left.scss'
+import '../css/_common.scss'
+import '../css/cave/cave_index.scss'
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
