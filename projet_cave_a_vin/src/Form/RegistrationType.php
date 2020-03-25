c4637f5d8240bc780ed3c1ab80444c6585623006
yomad96 <57949254+yomad96@users.noreply.github.com>
2020-03-25 09:03:04 +0100

    Adding placeholder and label for form

--- projet_cave_a_vin/src/Form/RegistrationType.php
+++ projet_cave_a_vin/src/Form/RegistrationType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class)
             ->add('password', PasswordType::class)
         ;
     }
