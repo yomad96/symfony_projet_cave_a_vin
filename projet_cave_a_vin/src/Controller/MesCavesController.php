4a6474b9bcf96dcd669b15582be81b3fe3c909ff
yomad96 <57949254+yomad96@users.noreply.github.com>
2020-03-25 11:20:21 +0100

    Adding cave when user is connected

--- projet_cave_a_vin/src/Controller/MesCavesController.php
+++ projet_cave_a_vin/src/Controller/MesCavesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Cave;
 use App\Entity\User;
+use App\Form\CaveType;
 use App\Repository\CaveRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,11 +14,24 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class MesCavesController extends AbstractController
 {
     /**
-     * @Route("/user/mes_caves", name="mes_caves", methods={"GET"})
+     * @Route("/user/mes_caves", name="mes_caves", methods={"GET","POST"})
      */
-    public function index(CaveRepository $caveRepository, UserInterface $user)
+    public function index(CaveRepository $caveRepository, UserInterface $user, Request $request)
     {
         $userId = $user->getId();
+
+        $cave = new Cave();
+        $form = $this->createForm(CaveType::class, $cave);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $user->addCave($cave);
+            $entityManager->persist($cave);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('mes_caves');
+        }
 
         return $this->render('mes_caves/index.html.twig', [
             'controller_name' => 'MesCavesController',
