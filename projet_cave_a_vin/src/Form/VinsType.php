8051c2b71075011fe3f723b5b75a9cb52de8a116
yomad96 <57949254+yomad96@users.noreply.github.com>
2020-03-26 11:21:19 +0100

    Changing label field on form VinsType

--- projet_cave_a_vin/src/Form/VinsType.php
+++ projet_cave_a_vin/src/Form/VinsType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Vins;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +13,12 @@ class VinsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Name')
-            ->add('Productor')
-            ->add('Appelation')
-            ->add('Cepage')
-            ->add('Milesime')
-            ->add('Region')
+            ->add('Name', TextType::class, array('label' => 'Nom'))
+            ->add('Productor',TextType::class, array('label' => 'Producteur'))
+            ->add('Appelation',TextType::class, array('label' => 'Appelation'))
+            ->add('Cepage',TextType::class, array('label' => 'Cépage'))
+            ->add('Milesime',TextType::class, array('label' => 'Milésime'))
+            ->add('Region',TextType::class, array('label' => 'Région'))
             ->add('Couleurs')
             ->add('quantite')
         ;
