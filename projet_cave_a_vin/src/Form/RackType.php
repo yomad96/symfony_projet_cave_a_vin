5a923fb99029b41f69a0fd601ae88c36ab16c527
yomad96 <57949254+yomad96@users.noreply.github.com>
2020-04-01 19:14:25 +0200

    rack : add choice type for colonne and ligne (prévisoire)

--- projet_cave_a_vin/src/Form/RackType.php
+++ projet_cave_a_vin/src/Form/RackType.php
@@ -7,6 +7,7 @@ use App\Entity\Rack;
 use App\Repository\CaveRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,10 +15,18 @@ class RackType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $arrayLigne = [0,1,2,3,4,5,6,7,8,9,10];
+        $arrayColonne = [0,1,2,3,4,5,6,7,8,9,10];
         $builder
             ->add('nom')
-            ->add('ligneTotal')
-            ->add('colonneTotal')
+            ->add('ligneTotal',ChoiceType::class,[
+                'choices' => $arrayLigne,
+                'label' => 'Nombre de ligne du rack'
+            ])
+            ->add('colonneTotal', ChoiceType::class, [
+                'choices' => $arrayColonne,
+                'label' => 'Nombre de colonne du rack'
+            ])
             ->add('cave',EntityType::class, [
                 "class" => Cave::class,
                 'query_builder' => function (CaveRepository $cave) {
