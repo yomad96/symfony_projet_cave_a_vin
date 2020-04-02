fae4e1476f1382535cbbffb2b9b9ba73981e9e12
yomad96 <57949254+yomad96@users.noreply.github.com>
2020-04-02 08:23:07 +0200

    mon_profil : add doughnuts

--- projet_cave_a_vin/src/Controller/MonProfilController.php
+++ projet_cave_a_vin/src/Controller/MonProfilController.php
@@ -2,18 +2,38 @@
 
 namespace App\Controller;
 
+use App\Repository\CaveRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class MonProfilController extends AbstractController
 {
     /**
      * @Route("/profil", name="mon_profil")
      */
-    public function index()
+    public function index(UserInterface $user, CaveRepository $caveRepository)
     {
+        $array = [];
+        $userId = $user->getId();
+        $caves = $caveRepository->findByUserId($userId);
+        foreach ($caves as $cave)
+        {
+            $vins = $cave->getVins();
+            foreach ($vins as $vin)
+            {
+                $vinQte = $vin->getQuantite();
+                array_push($array, $vinQte->getQuantity());
+            }
+
+        }
+
+
         return $this->render('mon_profil/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'caves' => $caves,
+            'array' => $array
+
         ]);
     }
 }
